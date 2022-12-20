df8d40f3a5ac22a53affe1be9a90bb8ae8bbf545
Leonardo Monteiro de Sousa <leonardo_04monteiro@hotmail.com>
2022-12-20 00:11:58 -0300

    adiciona opções de exclusão de listas To-do #6

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,5 +1,6 @@
 class TodoList < ApplicationRecord
     # has_many indica uma associação 1 para vários entre todo_lists e todo_items
     # ou seja, cada instância de todo_list pode ter 0, 1 ou várias instâncias de todo_item
-    has_many :todo_items
+    # Caso uma lista seja excluída, seus itens também serão.
+    has_many :todo_items, dependent: :destroy
 end
